6fc15208f1db9a05c90f292ce9bb6ae71f301247
_TaoDev_ <taodev3@proton.me>
2023-12-26 20:41:06 +0100

    [+] CLIs

--- functions.py
+++ functions.py
@@ -1,13 +1,19 @@
 from pyfiglet import Figlet
 import json
+import httpx
 
 def display_logo() -> None:
     """Display Jupiter CLI logo."""
     print("\033c\n", end="")
     print("-" * 51, "\n" + Figlet(font='small').renderText('JUPITER  CLI\n') + "-" * 51 + "\n")
     
-
 def load_wallets() -> dict:
     """Returns all wallets stored in wallets.json."""
     with open('wallets.json', 'r') as wallets_file:
         return json.load(wallets_file)
+    
+def get_crypto_price(crypto: str) -> float:
+    """Returns crypto price."""
+    API_BINANCE = f"https://www.binance.com/api/v3/ticker/price?symbol={crypto}USDT"
+    crypto_price =float(httpx.get(API_BINANCE).json()['price'])
+    return crypto_price
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import json
 import base58
 import os
+import time
+import re
+import httpx
 
 from dotenv import load_dotenv
 
 from InquirerPy import inquirer
 
+from tabulate import tabulate
+import pandas as pd
+
 from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+
+from solana.rpc.api import Client
 
 
 import functions as f
@@ -22,14 +31,14 @@ class Config_CLI():
             return json.load(config_file)
         
     @staticmethod
-    def edit_config_file(config_data: dict) -> dict:
-        """Edit config file.
-        Returns: dict"""
+    def edit_config_file(config_data: dict):
+        """Edit config file."""
         with open('config.json', 'w') as config_file:
             json.dump(config_data, config_file, indent=4)
+        return True
     
     @staticmethod
-    def collect_fees():
+    def prompt_collect_fees():
         """Asks the user if they want the CLI to take a small percentage of fees during their swaps."""
         collect_fees = inquirer.select(message="Would you like CLI to collect small fees from your swaps? (0.005%)", choices=["Yes", "No"]).execute()
         confirm = inquirer.select(message="Confirm?", choices=["Yes", "No"]).execute()
@@ -37,30 +46,88 @@ class Config_CLI():
             config_data = Config_CLI.get_config_data()
             config_data['COLLECT_FEES'] = True if collect_fees == "Yes" else False
             Config_CLI.edit_config_file(config_data=config_data)
+            return
         elif confirm == "No":
-            Config_CLI.collect_fees()
+            Config_CLI.prompt_collect_fees()
+            return
 
     @staticmethod
-    def rpc_url():
+    def prompt_rpc_url():
         """Asks the user the RPC URL endpoint to be used."""
-        rpc_url = inquirer.text(message="Enter your RPC URL endpoint:").execute()
-        confirm = inquirer.select(message="Confirm?", choices=["Yes", "No"]).execute()
+        # rpc_url = inquirer.text(message="Enter your RPC URL endpoint:").execute()
+        # confirm = inquirer.select(message="Confirm?", choices=["Yes", "No"]).execute()
+        rpc_url = os.getenv('RPC_URL')
+        confirm = "Yes"
         if confirm == "Yes":
-            config_data = Config_CLI.get_config_data()
-            config_data['RPC_URL'] = rpc_url
-            Config_CLI.edit_config_file(config_data=config_data)
+            if rpc_url.endswith("/"):
+                rpc_url = rpc_url[:-1]
+                
+            test_client = Client(endpoint=rpc_url)
+            
+            if not test_client.is_connected():
+                print("! Connection to RPC failed. Please enter a valid RPC.")
+                Config_CLI.prompt_rpc_url()
+                return
+            else:
+                config_data = Config_CLI.get_config_data()
+                config_data['RPC_URL'] = rpc_url
+                Config_CLI.edit_config_file(config_data=config_data)
+                return
+        
         elif confirm == "No":
-            Config_CLI.rpc_url()
-    
+            Config_CLI.prompt_rpc_url()
+            return
+            
+    @staticmethod
+    def main_menu():
+        """Main menu for CLI configuration."""
+        f.display_logo()
+        print("[CLI CONFIGURATION]\n")
+        config_data = Config_CLI.get_config_data()
+        # print(f"CLI collect fees (0.005%): {'Yes' if config_data['COLLECT_FEES'] else 'No'}") # TBD
+        print(f"RPC URL Endpoint: {config_data['RPC_URL']}")
+        print()
+        
+        config_cli_prompt_main_menu = inquirer.select(message="Select CLI parameter to change:", choices=[
+            # "CLI collect fees", # TBD
+            "RPC URL Endpoint",
+            "Back to main menu"
+        ]).execute()
+        
+        if config_cli_prompt_main_menu == "CLI collect fees":
+            Config_CLI.prompt_collect_fees()
+            Config_CLI.main_menu()
+        elif config_cli_prompt_main_menu == "RPC URL Endpoint":
+            Config_CLI.prompt_rpc_url()
+            Config_CLI.main_menu()
+        elif config_cli_prompt_main_menu == "Back to main menu":
+            Main_CLI.main_menu()
+
 
 class Wallet():
-    def __init__(self, private_key: str) -> None:
-        pass
+    def __init__(self, private_key: str):
+        self.wallet = Keypair.from_bytes(base58.b58decode(private_key))
     
 
-class Jupiter_CLI():
-    def __init__(self) -> None:
-        pass
+class Jupiter_CLI(Wallet):
+    
+    def __init__(self, private_key: str) -> None:
+        super().__init__(private_key=private_key)
+    
+    def main_menu(self):
+        """Main menu for Jupiter CLI."""
+        f.display_logo()
+
+        jupiter_cli_prompt_main_menu = inquirer.select(message="Select menu:", choices=[
+            "Swap",
+            "Limit Order",
+            "DCA",
+            "Snipe Token",
+            "Lookup",
+            "Stats",
+            "Select Wallet",
+            "Back to main menu",
+        ]).execute()
     
     
 class Wallets_CLI():
@@ -75,10 +142,10 @@ class Wallets_CLI():
             return json.load(wallets_file) 
     
     @staticmethod
-    def add_wallet():
+    def prompt_add_wallet():
         """Adds a wallet to wallets.json."""
-        wallet_private_key = inquirer.secret(message="Enter Wallet Private Key:").execute()
-        wallet_private_key = os.environ['PRIVATE_KEY']
+        # wallet_private_key = inquirer.secret(message="Enter Wallet Private Key:").execute()
+        wallet_private_key = os.getenv('PRIVATE_KEY')
         
         if wallet_private_key != "":
             try:
@@ -86,7 +153,8 @@ class Wallets_CLI():
                 pubkey = keypair.pubkey()
             except:
                 print("! Invalid private key.")
-                Wallets_CLI.add_wallet()
+                Wallets_CLI.prompt_add_wallet()
+                return
             
             confirm = inquirer.select(message=f"Wallet Address: {pubkey.__str__()}\nConfirm?", choices=["Yes", "No"]).execute()
             # confirm = "Yes"
@@ -100,7 +168,7 @@ class Wallets_CLI():
                 wallet_data = {
                         'wallet_name': wallet_name,
                         'pubkey': pubkey.__str__(),
-                        'keypair': wallet_private_key,
+                        'private_key': wallet_private_key,
                 }
                 wallets_data[len(wallets_data) + 1] = wallet_data
                 
@@ -108,35 +176,213 @@ class Wallets_CLI():
                     json.dump(wallets_data, wallets_file, indent=4)
                     
             elif confirm == "No":
-                Wallets_CLI.add_wallet()
-                
+                Wallets_CLI.prompt_add_wallet()
+                return
     
+    @staticmethod
+    def prompt_edit_wallet_name():
+        """Prompts user to edit a wallet name."""
+        wallets = Wallets_CLI.get_wallets()
+        choices = []
+        
+        for wallet_id, wallet_data in wallets.items():
+            choices.append(f"ID {wallet_id} - {wallet_data['wallet_name']} - {wallet_data['pubkey']}")
             
+        prompt_select_wallet_to_edit_name = inquirer.select(message="Select choice:", choices=choices).execute()
+        
+        confirm = inquirer.select(message="Confirm wallet selected?", choices=["Yes", "No"]).execute()
+        if confirm == "Yes":
+            new_wallet_name = inquirer.text(message="Enter Wallet new name:").execute()
+            
+            confirm = inquirer.select(message="Confirm wallet new name?", choices=["Yes", "No"]).execute()
+            if confirm == "Yes":
+                wallet_id = re.search(r'ID (\d+) -', prompt_select_wallet_to_edit_name).group(1)
+                wallets[wallet_id]['wallet_name'] = new_wallet_name
+                with open('wallets.json', 'w') as wallets_file:
+                    json.dump(wallets, wallets_file, indent=4)
+                return 
+            elif confirm == "No":
+                Wallets_CLI.prompt_edit_wallet_name()
+                return
+            
+        elif confirm == "No":
+            Wallets_CLI.prompt_edit_wallet_name()
+            return
+    
+    @staticmethod
+    def prompt_delete_wallet():
+        """Prompts user to delete wallet(s)"""
+        wallets = Wallets_CLI.get_wallets()
+        choices = []
+        
+        for wallet_id, wallet_data in wallets.items():
+            choices.append(f"ID {wallet_id} - {wallet_data['wallet_name']} - {wallet_data['pubkey']}")
+    
+        prompt_wallets_to_delete = inquirer.checkbox(message="Select wallet(s) to delete with SPACEBAR or press ENTER:", choices=choices).execute()
+        
+        if len(prompt_wallets_to_delete) == 0:
+            Wallets_CLI.main_menu()
+            return
+        else:
+            confirm = inquirer.select(message="Confirm delete wallet(s) selected?", choices=["Yes", "No"]).execute()
+            if confirm == "Yes":
+                for wallet_to_delete in prompt_wallets_to_delete:
+                    wallet_id = re.search(r'ID (\d+) -', wallet_to_delete).group(1)
+                    del wallets[wallet_id]
 
+                with open('wallets.json', 'w') as wallets_file:
+                    json.dump(wallets, wallets_file, indent=4)
+                return
+            
+            elif confirm == "No":
+                Wallets_CLI.main_menu()
+                return
+            
+    @staticmethod
+    def display_wallets():
+        print()
+        data = {
+            'ID': [],
+            'NAME': [],
+            'SOL BALANCE': [],
+            'ADDRESS': [],
+        }
+        wallets = Wallets_CLI.get_wallets()
+        client = Client(endpoint=Config_CLI.get_config_data()['RPC_URL'])
+        sol_price = f.get_crypto_price(crypto='SOL')
+        
+        for wallet_id, wallet_data in wallets.items():
+            data['ID'].append(wallet_id)
+            data['NAME'].append(wallet_data['wallet_name'])
+            data['ADDRESS'].append(wallet_data['pubkey'])
+            sol_balance = round(client.get_balance(pubkey=Pubkey.from_string(wallet_data['pubkey'])).value / 10 ** 9, 4)
+            sol_balance_usd = round(sol_balance * sol_price, 2)
+            data['SOL BALANCE'].append(f"{sol_balance} (${sol_balance_usd})")
+            
+        dataframe = tabulate(pd.DataFrame(data), headers="keys", tablefmt="fancy_grid", showindex="never", numalign="center")
+        print(dataframe)
+        print()
+        return
 
+    @staticmethod 
+    def main_menu():
+        """Main menu for Wallets CLI."""
+        f.display_logo()
+        print("[MANAGE WALLETS]")
+        Wallets_CLI.display_wallets()
+
+        wallets_cli_prompt_main_menu = inquirer.select(message="Select choice:", choices=[
+            "Add wallet",
+            "Edit wallet name",
+            "Delete wallet(s)",
+            "Back to main menu"
+        ]).execute()
+        
+        if wallets_cli_prompt_main_menu == "Add wallet":
+            Wallets_CLI.prompt_add_wallet()
+            Wallets_CLI.main_menu()
+            return
+        elif wallets_cli_prompt_main_menu == "Edit wallet name":
+            Wallets_CLI.prompt_edit_wallet_name()
+            Wallets_CLI.main_menu()
+            return
+        elif wallets_cli_prompt_main_menu == "Delete wallet(s)":
+            Wallets_CLI.prompt_delete_wallet()
+            Wallets_CLI.main_menu()
+            return
+        elif wallets_cli_prompt_main_menu ==  "Back to main menu":
+            Main_CLI.main_menu()
+            return
+        
+            
 class Main_CLI():
-    wallet = None
     
     def __init__(self):
-        if len(f.load_wallets()) > -1:
+        config_data = Config_CLI.get_config_data()
+        
+        if config_data['FIRST_LOGIN'] is True:
             Main_CLI.first_login()
-            Wallets_CLI.add_wallet()
+            Wallets_CLI.prompt_add_wallet()
+            
+            config_data['FIRST_LOGIN'] = False
+            config_data['LAST_WALLET_SELECTED'] = 1
+            Config_CLI.edit_config_file(config_data=config_data)
+        
         Main_CLI.main_menu()
         
     @staticmethod
     def first_login():
-        """Setting up CLI configuration when it's the first login."""
+        """Setting up CLI configuration if it's the first login."""
         
         f.display_logo()
         print("Welcome to the Jupiter Python CLI v.0.0.1! Made by @_TaoDev_")
         print("This is your first login, let's setup the CLI configuration.")
         
-        # Config_CLI.collect_fees()
-        # Config_CLI.rpc_url()
+        # Config_CLI.prompt_collect_fees() # TBD
+        Config_CLI.prompt_rpc_url()
+        
+        return
         
     @staticmethod
     def main_menu():
-        pass
+        """Main menu for CLI."""
+        f.display_logo()
+        print("Welcome to the Jupiter Python CLI v.0.0.1! Made by @_TaoDev_\n")
+        cli_prompt_main_menu = inquirer.select(message="Select menu:", choices=[
+            "Jupiter Exchange",
+            "Manage Wallets",
+            "CLI Configuration",
+            "About",
+            "Exit CLI"
+        ]).execute()
+        
+        if cli_prompt_main_menu == "Jupiter Exchange":
+            config_data = Config_CLI.get_config_data()
+            last_wallet_selected = Wallets_CLI.get_wallets()[str(config_data['LAST_WALLET_SELECTED'])]['private_key']
+            Jupiter_CLI(private_key=last_wallet_selected).main_menu()
+            return
+        elif cli_prompt_main_menu == "Manage Wallets":
+            Wallets_CLI.main_menu()
+            return
+        elif cli_prompt_main_menu == "CLI Configuration":
+            Config_CLI.main_menu()
+            return
+        elif cli_prompt_main_menu == "About":
+            print()
+            print("DESCRIPTION")
+            description = (
+                "This tool is a commande-line interface to use Jupiter Exchange faster made by @_TaoDev_." + 
+                "\nIt allows you to manage your wallets quickly, executes swaps, managing limit orders and DCA accounts, fetch wallet data (open orders, trades history...), tokens stats, and more!"
+            )
+            input(description)
+            print()
+            print("DISCLAIMER")
+            disclaimer = (
+                "Please note that the creator of this tool is not responsible for any loss of funds, damages, or other libailities resulting from the use of this software or any associated services." + 
+                "\nThis tool is provided for educational purposes only and should not be used as financial advice, it is still in expiremental phase so use it at your own risk."
+            )
+            input(disclaimer)
+            print()
+            print("CONTRIBUTIONS")
+            contributions = (
+                "If you are interesting in contributing, fork the repository and submit a pull request in order to merge your improvements into the main repository." + 
+                "\nContact me for any inquiry, I will reach you as soon as possible." +
+                "\nDiscord: _taodev_ | Twitter: @_TaoDev_ | Github: 0xTaoDev"
+            )
+            input(contributions)
+            print()
+            print("DONATIONS")
+            print("This project doesn't include platform fees.\nIf you find value in it and would like to support its development, your donations are greatly appreciated.")
+            cli_prompt_main_menu = inquirer.select(message="Would you make a donation?", choices=[
+                "Yes",
+                "No",
+            ]).execute()
+           
+            Main_CLI.main_menu()
+            return
+        elif cli_prompt_main_menu == "Exit CLI":
+            print("\nBye!")
+            time.sleep(1)
         
 
 if __name__ == "__main__":
